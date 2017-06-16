f45af5120a2e36f82035aa2e2c2dde1a983cc68a
André Zenzen <aaz@codeo.com.br>
2017-06-16 09:36:34 -0300

    Create routes for login and home Create controller and templates

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -17,3 +17,61 @@ angular.module('myApp', [
     'ui.mask',
 ])
 
+.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/home/');
+    $stateProvider
+    .state('home',{
+        url: '/home',
+        templateUrl:'templates/home.html',
+        controller: 'ProjectHomeController as ProjectHome'
+    })
+    .state('login', {
+        url: '/login',
+        controller:'ProjectLoginController as projectLogin',
+        templateUrl:'templates/login.html'
+    })
+})
+
+// Home Controller
+.controller("ProjectHomeController", function($scope, usuariosService, $location) {
+    $scope.logout = function(){
+        usuariosService.logout();
+    }
+})
+
+// Login Controller
+.controller("ProjectLoginController", function($rootScope, $scope, usuariosService, $location, $state) {
+  $scope.user = {
+      email:'',
+      password:''
+  }
+  //$scope.$apply();
+  $scope.submit = function(){
+   
+    if($scope.signinForm.$valid){
+        usuariosService.validaLogin($scope.user)
+        .then(function(firebaseUser){
+            //login success
+            $rootScope.usuarioLogado = firebaseUser;
+            $location.path('/home/');
+        })
+        .catch(function(error) {
+            //login falid
+            $rootScope.usuarioLogado = error;
+            $scope.loginError = true;
+        });
+    }else{
+        angular.forEach($scope.user,function(value,key) {
+            if(value && value.length == 0){
+                $scope.signinForm[key].$dirty = true;
+                $scope.signinForm[key].$error.required = true;
+            }
+        }, this);
+    }
+  }
+
+  $scope.register = function(){
+     $location.path('/registro');
+    //$state.go('registro');
+  }
+})
